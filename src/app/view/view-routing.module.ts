e7bdd31566103395f644f91ec5db7fae01228f9d
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-10 08:54:06 -0300

    update: fix view component

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ArgprogComponent } from './pages/argprog/argprog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { SkillsComponent } from './pages/skills/skills.component';
@@ -26,6 +29,18 @@ const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
