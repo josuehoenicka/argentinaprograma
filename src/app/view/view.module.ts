ad5714a5bae495e5e1cb4c2e6a0210d0fe29f889
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-10 19:28:08 -0300

    update: fix view component

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { FormsModule } from '@angular/forms';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { MaintenanceComponent } from './pages/maintenance/maintenance.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { NotfoundComponent } from './pages/notfound/notfound.component';
     NavComponent,
     LoginComponent,
     RegisterComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    MaintenanceComponent
   ],
   imports: [
     CommonModule,
