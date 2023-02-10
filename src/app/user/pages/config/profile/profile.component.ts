4c742fcbc465140b9aac3202bc7b8c20498bdbb9
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-10 20:22:45 -0300

    update: fix view component

--- src/app/user/pages/config/profile/profile.component.ts
+++ src/app/user/pages/config/profile/profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PortfolioService } from 'src/app/services/portfolio.service';
+import {Location} from '@angular/common';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  name:any;
+  experience:any;
+  certification:any;
+  constructor(private check:PortfolioService, private _location: Location) { }
+
+  backClicked() {
+    this._location.back();
+  }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.experience=data.experience;
+      this.certification=data.education;
+    });
   }
 
 }
