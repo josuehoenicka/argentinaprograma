7329ea5aa847118923813db28f7345af9573718a
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-10 08:53:11 -0300

    update: fix view component

--- src/app/view/pages/contact/contact.component.ts
+++ src/app/view/pages/contact/contact.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  name:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+    });
   }
 
 }
