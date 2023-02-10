a32c75ba9bc017ceb77ec380f97f81d3637ef686
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-10 19:33:45 -0300

    update: fix view component

--- src/app/view/pages/maintenance/maintenance.component.ts
+++ src/app/view/pages/maintenance/maintenance.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-maintenance',
   templateUrl: './maintenance.component.html',
-  styleUrls: ['./maintenance.component.css']
+  styleUrls: ['./maintenance.component.scss']
 })
 export class MaintenanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location)
+  {}
 
+  backClicked() {
+    this._location.back();
+  }
   ngOnInit(): void {
   }
 
