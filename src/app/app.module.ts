fae4da56958ab9da6f5c7d7e6081d2d49203bd7f
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-11 11:02:34 -0300

    update: added lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,76 +4,12 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ViewModule } from './view/view.module';
-
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { MainComponent } from './components/main/main.component';
-import { HttpClientModule } from "@angular/common/http";
-import { ConfigProfileComponent } from './components/config-profile/config-profile.component';
-import { ConfigSkillsComponent } from './components/config-skills/config-skills.component';
-import { ConfigProjectsComponent } from './components/config-projects/config-projects.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { SocialComponent } from './components/social/social.component';
-import { HomeUserComponent } from './components/home-user/home-user.component';
-import { HeaderUserComponent } from './components/header-user/header-user.component';
-import { AboutUserComponent } from './components/about-user/about-user.component';
-import { SkillsUserComponent } from './components/skills-user/skills-user.component';
-import { ProjectsUserComponent } from './components/projects-user/projects-user.component';
-import { SocialUserComponent } from './components/social-user/social-user.component';
-import { MainUserComponent } from './components/main-user/main-user.component';
-import { HeroUserComponent } from './components/hero-user/hero-user.component';
-import { BuildingComponent } from './components/building/building.component';
-import { ArgprogComponent } from './components/argprog/argprog.component';
-import { NoprojectsComponent } from './components/noprojects/noprojects.component';
-import { ConfigLangsComponent } from './components/config-langs/config-langs.component';
-import { ConfigEducationComponent } from './components/config-education/config-education.component';
-import { AddEducationComponent } from './components/add-education/add-education.component';
-import { AddExperienceComponent } from './components/add-experience/add-experience.component';
-import { AddProjectsComponent } from './components/add-projects/add-projects.component';
-import { ConfigExperiencesComponent } from './components/config-experiences/config-experiences.component';
-import { SkillsComponent } from './components/skills/skills.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HeroComponent,
-    MainComponent,
-    FooterComponent,
-    ConfigProfileComponent,
-    ConfigSkillsComponent,
-    ConfigProjectsComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    AboutComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    SocialComponent,
-    HomeUserComponent,
-    HeaderUserComponent,
-    AboutUserComponent,
-    SkillsUserComponent,
-    ProjectsUserComponent,
-    SocialUserComponent,
-    MainUserComponent,
-    HeroUserComponent,
-    BuildingComponent,
-    ArgprogComponent,
-    NoprojectsComponent,
-    ConfigLangsComponent,
-    ConfigEducationComponent,
-    AddEducationComponent,
-    AddExperienceComponent,
-    AddProjectsComponent,
-    ConfigExperiencesComponent,
   ],
   imports: [
     BrowserModule,
