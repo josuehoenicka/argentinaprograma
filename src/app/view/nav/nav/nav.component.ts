941744f567575782a9684807284d169862bac925
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-15 06:49:24 -0300

    update: design mobile

--- src/app/view/nav/nav/nav.component.ts
+++ src/app/view/nav/nav/nav.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent  {
 
-  constructor() { }
+  activeMenu = false;
 
-  ngOnInit(): void {
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
   }
 
 }
