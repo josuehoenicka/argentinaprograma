b14a28757eb63949b17437a44b685fef39cfc1f7
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-19 11:37:35 -0300

    update: fix mobile design

--- src/app/user/pages/about/about.component.spec.ts
+++ src/app/user/pages/about/about.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutComponent } from './about.component';
+import { UserAboutComponent } from './about.component';
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+describe('UserAboutComponent', () => {
+  let component: UserAboutComponent;
+  let fixture: ComponentFixture<UserAboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ UserAboutComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AboutComponent);
+    fixture = TestBed.createComponent(UserAboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
